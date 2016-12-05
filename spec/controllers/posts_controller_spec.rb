21603f06557f76ac37dcf514ea1abc8eb2565b1f
Andrew <andrewmbyrd@gmail.com>
2016-12-05 13:20:45 -0600

    Added create and read functionality

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,13 +15,27 @@ RSpec.describe PostsController, type: :controller do
 # #9
       expect(assigns(:posts)).to eq([my_post])
     end
+  #end get index
   end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, {id: my_post.id}
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the #show view" do
+ # #17
+       get :show, {id: my_post.id}
+       expect(response).to render_template :show
+     end
+
+     it "assigns my_post to @post" do
+       get :show, {id: my_post.id}
+ # #18
+       expect(assigns(:post)).to eq(my_post)
+     end
+
   end
 
   describe "GET #new" do
@@ -29,7 +43,42 @@ RSpec.describe PostsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
-  end
+
+    # #2
+     it "renders the #new view" do
+       get :new
+       expect(response).to render_template :new
+     end
+
+# #3
+     it "instantiates @post" do
+       get :new
+       expect(assigns(:post)).not_to be_nil
+     end
+
+  # end GET new
+   end
+
+   describe "POST create" do
+# #4
+     it "increases the number of Post by 1" do
+       expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+     end
+
+# #5
+     it "assigns the new post to @post" do
+       post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+       expect(assigns(:post)).to eq Post.last
+     end
+
+# #6
+     it "redirects to the new post" do
+       post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+       expect(response).to redirect_to Post.last
+     end
+  #end POST create
+   end
+
 
   describe "GET #edit" do
     it "returns http success" do
