fd8f135c15c252ecbf3001e0f5f0628cc04a5f08
Andrew <andrewmbyrd@gmail.com>
2016-12-05 11:24:27 -0600

    added CRUD for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,27 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'posts/index'
 
-  get 'welcome/about'
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  #we can refactor all of the GET methods for Post with the below
+  #get 'post/index'
+  #get 'post/show'
+  #get 'post/new'
+  #get 'post/edit'
+
+  resources :posts
+
+
+  #we don't need get welcome/index because it's the root
+  #get 'welcome/index'
+
+  #get 'welcome/about'
+  #this line directs to /about rather than welcome/about
+  get 'about' => 'welcome#about'
 
   get 'welcome/contact'
 
